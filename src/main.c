d85354cabf7324d42ace3ce4981a009bae6bc693
enukic <67737796+enukic@users.noreply.github.com>
2023-04-05 14:43:12 +0200

    fix build errors

--- src/main.c
+++ src/main.c
@@ -4,13 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
-#include <sys/printk.h>
-#include "img_mgmt/img_mgmt.h"
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 
 void main(void)
 {
-	img_mgmt_register_group();
-	printk("Test 2\n");
+	// img_mgmt_register_group(); // Not needed in ncs 2.3.0
+	printk("Test UPDATE\n");
 	printk("Application for Updating net core and app core (application and mcuboot)! \n");
 }
